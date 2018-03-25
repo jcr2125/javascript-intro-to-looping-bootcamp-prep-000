f14570903c41258d15328954cb60d0a2b2ec31c9
John Rodriguez <jcr2125@gmail.com>
2018-03-25 23:23:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop(arr){
-  for(var i = 0; i < 25; i++){
+  for(let i = 0; i < 25; i++){
+    console.log(i);
     arr.push(`I am ${i + 1} strange loop${i === 0 ? "" : "s"}.`);
     console.log(arr[i]);
   }
